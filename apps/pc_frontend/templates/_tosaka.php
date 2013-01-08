a4c0e5240cf227192d1bfac38e1753c0f786695c
tatsuya ichikawa <ichikawa@tejimaya.com>
2013-01-08 16:00:42 +0900

    投稿フォームがタイムラインプラグインを入れてない時は動作しないのを修正。
    入れてない状態をbasic-mode、入れている状態をtimeline-modeとし、
    jsでremove,showする

--- apps/pc_frontend/templates/_tosaka.php
+++ apps/pc_frontend/templates/_tosaka.php
@@ -78,21 +78,23 @@
     <textarea id="tosaka_postform_body" class="span12" rows="4" placeholder="<?php echo __('What are you doing now?') ?>"></textarea>
   </div>
   <div class="row">
-    <div id="timeline-submit-error"></div>
-    <input id="timeline-submit-upload" type="file" name="timeline-submit-upload" enctype="multipart/form-data">
+    <div id="timeline-submit-error" class="timeline-mode" style="display: none;"></div>
+    <input id="timeline-submit-upload" type="file" name="timeline-submit-upload" enctype="multipart/form-data" class="timeline-mode" style="display: none;">
     <?php if ($community): ?>
-    <button id="timeline_postform_submit" data-community-id="<?php echo $community->getId() ?>" class="span4 btn small btn-primary"><?php echo __('%post_activity%') ?></button>
+    <button id="tosaka_postform_submit" data-community-id="<?php echo $community->getId() ?>" class="span12 btn small btn-primary basic-mode"><?php echo __('%post_activity%') ?></button>
+    <button id="timeline_postform_submit" data-community-id="<?php echo $community->getId() ?>" class="span4 btn small btn-primary timeline-mode" style="display: none;"><?php echo __('%post_activity%') ?></button>
     <?php else: ?>
-    <button id="timeline_postform_submit" class="span4 btn small btn-primary"><?php echo __('%post_activity%') ?></button>
+    <button id="tosaka_postform_submit" class="span12 btn small btn-primary basic-mode"><?php echo __('%post_activity%') ?></button>
+    <button id="timeline_postform_submit" class="span4 btn small btn-primary timeline-mode" style="display: none;"><?php echo __('%post_activity%') ?></button>
     <?php endif; ?>
-    <span id="timeline-upload-photo-button" class="btn"><span class="icon-camera"></span></span>
+    <span id="timeline-upload-photo-button" class="btn timeline-mode" style="display: none;"><span class="icon-camera"></span></span>
     <span id="photo-file-name"></span>
-    <select id="timeline-public-flag">
+    <select id="timeline-public-flag" class="timeline-mode" style="display: none;">
           <?php foreach ($publicFlags as $value => $text): ?>
-            <option value="<?php echo $value ?>"><?php echo __($text) ?></option>
+            <option value="<?php echo $value ?>" style="display: none;" class="timeline-mode"><?php echo __($text) ?></option>
           <?php endforeach; ?>
     </select>
-    <div class="center hide" id="timelinePostLoading"><?php echo op_image_tag('ajax-loader.gif') ?></div>
+    <div class="center hide" id="timelinePostLoading" class="timeline-mode" style="display: none;"><?php echo op_image_tag('ajax-loader.gif') ?></div>
   </div>
 </div>
 <!-- POSTFORM TMPL -->
